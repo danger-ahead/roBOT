921d04a890e55c5699e886132ee9602a5b6f3756
danger-ahead <danger.ahead@pm.me>
2021-05-17 13:06:28 +0530

    added-meaning-module-free-dictionary-api

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import discord
 import os
 from decouple import config
+import  requests
+import json
 
 client = discord.Client()
 
@@ -13,8 +15,30 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('fuckYouSHOHOM'):
-        await message.channel.send('YEAH FUCK HIM')
+    if message.content.startswith('roBOT LETS GET TO WORK'):
+        await message.channel.send('HECK YEAH')
+
+    if message.content.startswith('_mean'):
+
+        word_list = message.content.split()
+
+        if(len(word_list) > 2): #user has more than 2 words as input
+            await message.channel.send('I can\'t search the meaning of 2 words simultaneously')
+
+        else:
+            st = ''
+            word = word_list[1]
+            url = 'https://api.dictionaryapi.dev/api/v2/entries/en_US/' + word
+            r = requests.get(url)
+            data = json.loads(r.text)[0]
+            pairs = data.items()
+            data2 = (data["meanings"])
+
+            for definitions in data2:
+                st = st + str(definitions)
+                st = st + '\n\n'
+            
+            await message.channel.send(st)   
 
 DISCORD_TOKEN=config('TOKEN')
 client.run(DISCORD_TOKEN)
