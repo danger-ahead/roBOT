8651669c55d10d38d49e2b0ca66a21f38fb3c8d0
danger-ahead <danger.ahead@pm.me>
2021-05-17 15:04:02 +0530

    meaning-2nd-commit

--- main.py
+++ main.py
@@ -23,22 +23,26 @@ async def on_message(message):
         word_list = message.content.split()
 
         if(len(word_list) > 2): #user has more than 2 words as input
-            await message.channel.send('I can\'t search the meaning of 2 words simultaneously')
+            await message.channel.send('Enter a single word at a time :/')
 
         else:
-            st = ''
             word = word_list[1]
-            url = 'https://api.dictionaryapi.dev/api/v2/entries/en_US/' + word
-            r = requests.get(url)
-            data = json.loads(r.text)[0]
-            pairs = data.items()
-            data2 = (data["meanings"])
+            try:
+                url = 'https://api.dictionaryapi.dev/api/v2/entries/en_US/' + word
+                r = requests.get(url)
+                data = json.loads(r.text)[0]
+                pairs = data.items()
+                data2 = (data["meanings"])
 
-            for definitions in data2:
-                st = st + str(definitions)
-                st = st + '\n\n'
-            
-            await message.channel.send(st)   
+                for definitions in data2:
+                    definitions2 = (definitions["definitions"])
+                    definitions2 = str(definitions2)
+                    definitions2 = definitions2.split("\'")
+                    define = definitions2[3]
+                    await message.channel.send(define)
+
+            except:
+                await message.channel.send('Didn\'t find the word :/')
 
 DISCORD_TOKEN=config('TOKEN')
 client.run(DISCORD_TOKEN)
