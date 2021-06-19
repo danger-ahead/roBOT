343e067910dba5ee564bfc438714f65f15fbcf1e
Shourya Shikhar <danger.ahead@pm.me>
2021-06-19 13:14:55 +0530

    fixed_rolldice

--- commands/scripts/games.py
+++ commands/scripts/games.py
@@ -1,54 +1,29 @@
 import random
 import discord
+import os
 
 
 async def roll_a_dice(discord, message):
     number = random.randint(1, 6)
+    cwd = os.getcwd()   #stores current directory before changing
+    os.chdir("scripts/images")  #changes to the images directory
 
     if number == 1:
-        embed = discord.Embed(
-            title="Dice!!",
-            description="|--------|\n|---0---|\n|--------|",
-            color=discord.Color.blue(),
-        )
-        await message.channel.send(embed=embed)
+        await message.reply(file=discord.File('dice1.jpg'))
     elif number == 2:
-        embed = discord.Embed(
-            title="Dice!!",
-            description="|-0---|\n|------|\n|---0-|",
-            color=discord.Color.blue(),
-        )
-        await message.channel.send(embed=embed)
+        await message.reply(file=discord.File('dice2.jpg'))
     elif number == 3:
-        embed = discord.Embed(
-            title="Dice!!",
-            description="|---------|\n|0--0--0|\n|---------|",
-            color=discord.Color.blue(),
-        )
-        await message.channel.send(embed=embed)
+        await message.reply(file=discord.File('dice3.jpg'))
     elif number == 4:
-        embed = discord.Embed(
-            title="Dice!!",
-            description="|0---0|\n|-------|\n|0---0|",
-            color=discord.Color.blue(),
-        )
-        await message.channel.send(embed=embed)
+        await message.reply(file=discord.File('dice4.jpg'))
     elif number == 5:
-        embed = discord.Embed(
-            title="Dice!!",
-            description="|0----0|\n|---0---|\n|0----0|",
-            color=discord.Color.blue(),
-        )
-        await message.channel.send(embed=embed)
+        await message.reply(file=discord.File('dice5.jpg'))
     else:
-        embed = discord.Embed(
-            title="Dice!!",
-            description="|0 0 0|\n|------|\n|0 0 0|",
-            color=discord.Color.blue(),
-        )
-        await message.channel.send(embed=embed)
+        await message.reply(file=discord.File('dice6.jpg'))
 
     await message.add_reaction("\U0001f44d")
+
+    os.chdir(cwd)   #changes back to the previously stored directory
 
 
 async def toss_coin(discord, message):
