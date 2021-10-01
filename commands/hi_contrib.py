73efff8e9b086063d0cb6d13bdf578a7d06f142b
smart-worker <sarkarsoham07@gmail.com>
2021-10-01 16:02:57 +0530

    Changed welcome message

--- commands/hi_contrib.py
+++ commands/hi_contrib.py
@@ -5,12 +5,12 @@ Module containing the functions for _hi and _contribute commands.
 
 async def hi(discord, message):
     embed = discord.Embed(
-        title="Hello comrade!!, Meet myself roBOT!",
-        description="an amatuer bot by amatuer Developers!! XD \
-        \n The full list of commands \
-        can be found here: \n https://github.com/RccTechz/roBOT/blob/master/docs/COMMANDS.md \n\
-        have a great time interacting and having fun with me!!\n for details about how to contribute to \
-        this bot use  '_contribute' ",
+        title="🙏🏻 Hello my friend!!, myself roBOT!",
+        description="an amatuer bot by amatuer Developers!! 😂 \
+        \n Want to play with me?\
+        \nVisit: https://github.com/danger-ahead/roBOT/blob/master/docs/COMMANDS.md \n\
+        to know all commands I understand. \nI hope you're still here. Looking forward to spend some quality time with you.  \
+        \nTo know how you can improve my functionality use  '_contribute' ",
         color=discord.Color.blue(),
     )
     await message.channel.send(embed=embed)
