fb3050c86bd4746c27e475de18758d70997a30fc
Senor-Ducky <robom460@gmail.com>
2021-10-05 21:38:43 +0530

    replaced requests with aiohttp and minor changes

--- command/api/mal.py
+++ command/api/mal.py
@@ -1,6 +1,6 @@
 import discord
 import json
-import requests
+import aiohttp
 from discord.ext import commands
 
 class MAL(commands.Cog):
@@ -8,16 +8,23 @@ class MAL(commands.Cog):
         self.client = client
 
     @commands.command()
-    async def anime(self, ctx, *args):
-        url = requests.get('https://api.jikan.moe/v3/search/anime?q={}&page=1'.format(' '.join(args)))
-        data = url.json()
-        await ctx.send(data['results'][0]['url'])
+    async def anime(self, ctx, *, args):
+        async with aiohttp.ClientSession() as session:
+            url = 'https://api.jikan.moe/v3/search/anime?q={}&page=1'.format(args)
+            response = await session.get(url)
+            data = await response.json()
+            await ctx.send(data['results'][0]['url'])
 
     @commands.command()
-    async def manga(self, ctx, *args):
-        url = requests.get('https://api.jikan.moe/v3/search/manga?q={}&page=1'.format(' '.join(args)))
-        data = url.json()
-        await ctx.send(data['results'][0]['url'])
+    async def manga(self, ctx, *, args):
+        async with aiohttp.ClientSession() as session:
+            url = 'https://api.jikan.moe/v3/search/manga?q={}&page=1'.format(args)
+            response = await session.get(url)
+            data = await response.json()
+            await ctx.send(data['results'][0]['url'])
+
+    
+        
         
     
 def setup(client):
