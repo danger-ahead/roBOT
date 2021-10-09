064016e7cc36f16beb861d35f2224bda482e86e3
Senor-Ducky <robom460@gmail.com>
2021-10-09 16:11:39 +0530

    improved stack search command to directly take text as input

--- command/api/stack.py
+++ command/api/stack.py
@@ -1,6 +1,8 @@
 import discord
 from stackapi import StackAPI
 from discord.ext import commands
+import requests
+import json
 
 class StackOverflow(commands.Cog):
     def __init__(self,client):
@@ -8,10 +10,15 @@ class StackOverflow(commands.Cog):
     
     @commands.command()
     async def stack_search(self, ctx, *, args):
-        SITE = StackAPI('stackoverflow')
-        results = SITE.fetch(f'questions/{args}')
-        await ctx.send(results['items'][0]['link'])
+        url = f'https://api.stackexchange.com//2.3/search/advanced?page=1&pagesize=1&order=desc&sort=activity&q={args} typeerror&site=stackoverflow'.replace(' ', '+')
+        req = requests.get(url)
+        data = req.json()
+        await ctx.send(data['items'][0]['link'])
+        
 
 def setup(client):
     client.add_cog(StackOverflow(client))
+
+
+
 
